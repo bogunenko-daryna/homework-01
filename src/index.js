f4279c7cab0fe6a19a275b6204f5e5ed8d3f4189
Daryna-dev <bogunenkodarina@gmail.com>
2021-08-19 22:29:49 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "../index.css";
+import JS_IMAGE from '../assets/js-img.png';
+
+console.log('Hello World!');
+
+const jsImageHtml = document.createElement('img')
+jsImageHtml.className = 'js-image';
+jsImageHtml.src = JS_IMAGE;
+
+document.body.append(jsImageHtml)
